4cab952ef9846e8041e07ac9fcf72ec428b38b49
Mayank Agarwal <arenmayank20@gmail.com>
2023-01-16 17:42:48 +0530

    Image slider on hover

---
+++ src/slider/Slider.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="main-container">
+        <div class="container">
+            <img v-for="(image,index) in images" v-bind:src="image" v-if="index===currentIndex" @mouseenter="imgHover"  />
+        </div>
+        <div class="indicator-container">
+                <span v-for="(image,index) in images" v-bind:class="{ active: (image,index) === currentIndex }"
+                    @mouseenter="currentIndex = index"></span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            images: [
+                'https://i.postimg.cc/QNYkPnvr/p1.jpg',
+                'https://i.postimg.cc/W1B5yfF4/p2.jpg',
+                'https://i.postimg.cc/h40PPkcH/p4.jpg',
+            ],
+            currentIndex:0,
+            // imgHover:false,
+        }
+    },
+    methods: {
+        imgHover(){
+            setInterval(()=>{
+                this.currentIndex = (this.currentIndex + 1 + this.images.length) % this.images.length;
+            },1000)
+        },
+
+    },
+}
+</script>
+
+<style scoped>
+.main-container{
+    margin: auto;
+    margin-top: 250px;
+    margin-left:200px ;
+    position: relative;
+}
+.container{
+    width: 320px;
+    height: 522px;
+}
+img {
+    position: absolute;
+    width: 320px;
+    height: 522px;
+}
+.indicator-container {
+    width: 320px;
+    height: 3px;
+    display: flex;
+}
+.indicator-container span {
+    width: 100%;
+    height: 3px;
+}
+ span.active {
+    transition: 0.3s ease-in;
+    width: 100%;
+    height: 3px;
+    background-color: black;
+    opacity: 1;
+}
+</style>
---
+++ src/slider/Slides.vue
@@ -0,0 +1,89 @@
+<template>
+        <transition-group name="fade" mode="out-in" tag="div" id="slides">
+            <div class="slideParent" :key="image.url">
+                <div class="slideImage" :style="setBackgroundImage" 
+                >
+                    <div class="slideTitle">
+                        {{image.Array}}
+                    </div>
+                </div>
+            </div>
+        </transition-group>
+</template>
+
+<script>
+export default {
+    props: ["image"],
+    computed:{
+        setBackgroundImage(){
+            return {backgroundImage : "url("+this.image.url+")"};
+        }
+    },
+}
+</script>
+
+<style>
+
+#slides .slideParent{
+    width: 100%;
+}
+#slides .slideImage{
+    width: 100%;
+
+    background-position:center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    animation-fill-mode: forwards;
+    animation-duration: 4s;
+    animation-iteration-count: 1; 
+}
+@keyframes zoom{
+    from{
+        transform: scale(1);
+    }
+    to{
+        transform: scale(1.1);
+    }
+}
+
+#slides .slideImage .slideTitle{
+    color:Red;
+    font-weight:bold;
+    font-size: 40px;
+    height:100vh;
+    display:flex;
+    justify-content: center;
+    align-items:center;
+}
+.fade-enter-active{
+    animation-name:fadeEnter;
+    animation-duration: 1s;
+    animation-iteration-count: 1;
+}
+.fade-move{
+    transition: 1s ease;
+}
+.fade-leave-active{
+    animation-name:fadeLeave;
+    animation-duration: 1s;
+    animation-iteration-count: 1;
+    position:absolute;
+    transition: 1s ease-out;
+}
+@keyframes fadeEnter{
+    from{
+        opacity:0;
+    }
+    to{
+        opacity:1;
+    }
+}
+@keyframes fadeLeave{
+    from{
+        opacity:1;
+    }
+    to{
+        opacity:0;
+    }
+}
+</style>
